afe57b81e683ec466a0343072090877410ca5935
Francis Kiryowa <franciskiryowa68@gmail.com>
2018-10-31 19:18:31 -0400

    Add README.md file

--- js/google.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("search").addEventListener("click",function(){
-    var search_term = document.getElementById("search_term").value;
-    if(search_term == "") {
-        alert("Please enter a search term");
-    }else {
-        alert(search_term);
-    }
-});
